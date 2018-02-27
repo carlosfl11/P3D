c87b3d9f553883a7182f9f4c5a16c0efbe33bd31
ninjanazal <32069840+ninjanazal@users.noreply.github.com>
2018-02-27 16:51:01 +0000

    null

--- game1/game1/rectangle.h
+++ game1/game1/rectangle.h
@@ -5,9 +5,11 @@
 namespace my_game {
 
 	class Rectangle {
+
 	protected:
 		float xc, yc;
 		float width, height;
+
 	public:
 		Rectangle(float x = 0.0f, float y = 0.0f, float w = 0.0f, float h = 0.0f) : xc(x), yc(y), width(w), height(h) {};
 	};
