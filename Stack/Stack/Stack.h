22d6e732230f585e06f1af7fe7cd542f3720bc60
ninjanazal <32069840+ninjanazal@users.noreply.github.com>
2018-03-06 17:46:04 +0000

    auto

---
+++ Stack/Stack/Stack.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+template <class Stack>
+class Stack
+{
+	int size
+	int *top;
+	auto [] s;
+public:
+	//construtor
+	Stack(int x): size(x){
+		if (x <= 0)
+			throw "Tamanho negativo!";
+		else
+			s
+			s = new auto[x];
+	}
+
+	//destrutor
+	~Stack() {
+		for (int i = 0; i < s ; i++)
+		{
+			
+		}
+	}
+
+
+};
+
