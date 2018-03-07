69777d73b0a39160e637332303d5fcaa73cfa14b
ninjanazal <32069840+ninjanazal@users.noreply.github.com>
2018-03-07 16:54:35 +0000

    update

--- Stack/Stack/Main.cpp
+++ Stack/Stack/Main.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include "Stack.h"
+#include <string>
 
 using namespace std;
 
-int main() {
-	Stack<int> *stack = 0;
-	// try / catch para exxcecoes
-	try
-	{
-		stack = new Stack<int>(10);
-	}
-	catch (int e)
-	{
-		if (e == 1)
-			cout << "Tamanho negativo\n";
-		if (e == 2)
-			cout << "Stack completa" << endl;
-	}
-	if (stack != NULL) {
-		cout << "tamanho: " << stack->getSize() << endl;
+void stack() {
+	Stack<int> stack(-1);
+}
 
-	}
+void stack2() {
+	Stack<int> stack2(5);
+
+	stack2.push(1);
+	stack2.push(2);
+	stack2.push(3);
+
+	stack2.print();
+
+}
+
+void stack3() {
+	Stack<string> stack3(4);
+
+	stack3.push("abc");
+	stack3.push("def");
+	stack3.push("ghi");
+
+	stack3.print();
+
+	stack3.pop();
+	stack3.print();
+
+
+}
+
+int main() {
+
+
+	// try / catch para exxcecoes
+	try { stack(); }
+	catch (int e) { if (e == 1)	cout << "Tamanho negativo\n"; }
+
+	stack2();
+	stack3();
+
 	cin.get();
 	return 0;
 }
--- Stack/Stack/Stack.h
+++ Stack/Stack/Stack.h
@@ -5,21 +5,60 @@
 using namespace std;
 
 template<class type>
-class Stack{
-	int size, topobject = 0;
-	type *stack;
+class Stack {
+	int size = 0, topobject = 0;
+	type *stack = nullptr;
 public:
-	Stack(int x) {
-		if (x <= 0)
-			throw 1;	// caso seja negativvo lanca exxcepcao
-		else {
-			size = x;
-			stack = new type[size];
-			cout << "Stack criada com tamanho " << size << endl;
-		}
-	}
-	~Stack() { delete[] stack; cout << "Stack Eliminada!" << endl; }
-	int getSize() { return size; }
+	Stack(const int x);
+	~Stack();
+	bool push(type n);
+	void pop();
+	void print();
 };
+
+template<class type> Stack<type>::Stack(const int x) {
+	if (x <= 0)
+		throw 1;
+	else
+	{
+		size = x;
+		stack = new type[x];
+		cout << "Stack criada com o tamanho " << size << endl;
+
+	}
+}
+
+template<class type> Stack<type>::~Stack() {
+	delete[] stack;
+	cout << "->Stack com o tamanho " << size << " eliminada" << endl;
+}
+
+template<class type> bool Stack<type>::push(type n) {
+	if (topobject < size)
+	{
+		stack[topobject] = n;
+		topobject++;
+		return true;
+	}
+	else
+	{
+		throw 2;
+		return false;
+	}
+}
+
+template<class type> void Stack<type>::pop() {
+	if (topobject > 0) {
+		topobject--;
+		cout << "->" << stack[topobject] << " eliminado!" << endl;
+	}
+}
+
+template<class type> void Stack<type>::print() {
+	for (int i = 0; i < topobject; i++)
+		cout << stack[i] << endl;
+	cout << "\n";
+
+}
 
 
